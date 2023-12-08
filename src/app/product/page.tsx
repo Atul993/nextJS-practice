8162e5fad3bdde18f0f2f86fae8315f9880aff23
Atul Dhotre <atuldhotre993@gmail.com>
2023-12-08 20:00:00 +0530

    dynamic page route add

---
+++ src/app/product/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList() {
+  return (
+    <>
+    <div> Product LIST</div>
+      <div>Product 1</div>
+      <div>Product 2</div>
+      <div>Product 3</div>
+    </>
+  );
+}
