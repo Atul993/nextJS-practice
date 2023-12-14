0394369f1ab65de2653d2fe598e70db6b936c02f
Atul Dhotre <atuldhotre993@gmail.com>
2023-12-14 20:00:00 +0530

    add conditionally not found page

--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
 export default function ReviewDetails({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) {
+  if (parseInt(params.productId) > 1000) {
+    notFound();
+  }
   return (
     <div>
       Review {params.reviewId} for product {params.productId}
