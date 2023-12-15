e66c7d056ddfe60473a839328951ab0b204f5dda
Atul Dhotre <atuldhotre993@gmail.com>
2023-12-15 20:00:00 +0530

    file colocation practice

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+function BarChart() {
+  return <h1>BarChart</h1>;
+}
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard page</h1>
+      <BarChart />
+    </div>
+  );
+}
