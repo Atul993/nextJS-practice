d689e21a26684f523763de4e81c5e6289e907604
Atul Dhotre <atuldhotre993@gmail.com>
2023-12-22 20:00:00 +0530

    generate async routeing metadata

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -6,9 +6,16 @@ type Props = {
   };
 };
 
-export const generateMetadata = ({ params }: Props): Metadata => {
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${params.productId}`);
+    }, 100);
+  });
   return {
-    title: `Product ${params.productId}`,
+    title: `Product ${title}`,
   };
 };
 
